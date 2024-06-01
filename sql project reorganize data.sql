5ad82b54f4bed9eabbce061d33734092f70a7711
El mahdi Laqlach <78571111+ELMAHDI37@users.noreply.github.com>
2024-06-01 01:47:16 +0100

    data cleaning SQL
    This SQL query aims to unpivot data from wide format (columns for each year) to long format (rows for each year) to facilitate visualization and analysis.

---
+++ sql project reorganize data.sql
@@ -0,0 +1,126 @@
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1960' AS Year, `1960` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1961' AS Year, `1961` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1962' AS Year, `1962` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1963' AS Year, `1963` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1964' AS Year, `1964` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1965' AS Year, `1965` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1966' AS Year, `1966` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1967' AS Year, `1967` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1968' AS Year, `1968` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1969' AS Year, `1969` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1970' AS Year, `1970` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1971' AS Year, `1971` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1972' AS Year, `1972` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1973' AS Year, `1973` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1974' AS Year, `1974` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1975' AS Year, `1975` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1976' AS Year, `1976` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1977' AS Year, `1977` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1978' AS Year, `1978` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1979' AS Year, `1979` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1980' AS Year, `1980` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1981' AS Year, `1981` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1982' AS Year, `1982` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1983' AS Year, `1983` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1984' AS Year, `1984` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1985' AS Year, `1985` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1986' AS Year, `1986` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1987' AS Year, `1987` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1988' AS Year, `1988` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1989' AS Year, `1989` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1990' AS Year, `1990` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1991' AS Year, `1991` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1992' AS Year, `1992` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1993' AS Year, `1993` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1994' AS Year, `1994` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1995' AS Year, `1995` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1996' AS Year, `1996` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1997' AS Year, `1997` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1998' AS Year, `1998` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '1999' AS Year, `1999` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2000' AS Year, `2000` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2001' AS Year, `2001` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2002' AS Year, `2002` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2003' AS Year, `2003` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2004' AS Year, `2004` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2005' AS Year, `2005` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2006' AS Year, `2006` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2007' AS Year, `2007` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2008' AS Year, `2008` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2009' AS Year, `2009` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2011' AS Year, `2011` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2012' AS Year, `2012` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2013' AS Year, `2013` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2014' AS Year, `2014` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2015' AS Year, `2015` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2016' AS Year, `2016` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2017' AS Year, `2017` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2018' AS Year, `2018` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2019' AS Year, `2019` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2020' AS Year, `2020` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2021' AS Year, `2021` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2022' AS Year, `2022` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+UNION ALL
+SELECT Country_Name, Country_Code, Indicator_Name, Indicator_Code, '2023' AS Year, `2023` AS Value FROM mon_projectb1.`api_sl.uem.totl.zs_ds2_en_csv_v2_60`
+ORDER BY Country_Name, Year;
